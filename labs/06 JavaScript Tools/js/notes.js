632ee972cd9e2a6c3067fe681acfe33f843c13d5
Oskari Ahonen <ahonenoskari@gmail.com>
2019-03-28 14:29:01 +0200

    tehtävä 6 palautus

--- labs/06 JavaScript Tools/js/notes.js
+++ labs/06 JavaScript Tools/js/notes.js
@@ -1,34 +1,65 @@
 'use strict'
 
-var notes = new Array();
+let notes = new Array();
 
 function addItem() {
-	let textbox = document.getElementById('item');
-	var itemText = textbox.value;
-	textbox.value = '';
-	textbox.focus();
-	var newItem = {title: itemText, quantity: 1};
-	notes.push(newItem);
-	displayList();
+    let textbox = document.getElementById('item');
+    let itemText = textbox.value;
+    let totuus = false;
+    textbox.value = '';
+    textbox.focus();
+
+    if (notes.length > 0) {
+        for (let i = 0; i < notes.length; i++) {
+            let tarkistus = notes[i].title;
+            if (itemText.localeCompare(tarkistus) === 0) {
+                notes[i].quantity++;
+                totuus = true;
+            }
+        }
+    }
+
+    if (!totuus){
+        let newItem = {title: itemText, quantity: 1};
+        notes.push(newItem);
+    }
+
+
+    displayList();
+    saveList();
 }
 
 function displayList() {
-	var table = document.getElementById('list');
-	table.innerHTML = '';
-	for (var i = 0; i<notes.length; i++) {
-		var node = undefined;
-		var note = notes[i];
-		var node = document.createElement('tr');
-		var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
-	    node.innerHTML = html;
-		table.appendChild(node);
-	}
+    let table = document.getElementById('list');
+    table.innerHTML = '';
+    for (let i = 0; i < notes.length; i++) {
+    /*    let node = undefined;*/
+        let note = notes[i];
+        let node = document.createElement('tr');
+        let html = '<td>' + note.title + '</td><td>' + note.quantity + '</td><td><a href="#" onClick="deleteIndex(' + i + ')">delete</td>';
+        {node.innerHTML = html;}
+        table.appendChild(node);
+    }
 }
 
 function deleteIndex(i) {
-	notes.splice(i, 1);
-	displayList();
+    notes.splice(i, 1);
+    displayList();
+    saveList();
 }
 
+loadList();
 let button = document.getElementById('add');
 button.onclick = addItem;
+
+function saveList() {
+    localStorage.notes = JSON.stringify(notes);
+}
+
+function loadList() {
+    console.log('loadList');
+    if (localStorage.notes) {
+        notes = JSON.parse(localStorage.notes);
+        displayList();
+    }
+}
